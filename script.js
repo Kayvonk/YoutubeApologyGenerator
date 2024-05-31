b91d65e2e224ca130c1d562a6115619ef03ed226
Kayvon Kazemini <kayvonk@gmail.com>
2024-05-31 06:43:00 -0600

    separate deploys

--- script.js
+++ script.js
@@ -73,11 +73,11 @@ app.post('/api/ask', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
