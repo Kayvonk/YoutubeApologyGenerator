bfa67d27d8e36eed3b97b389d8a862ee57c06c0a
Kayvon Kazemini <kayvonk@gmail.com>
2024-05-31 05:35:18 -0600

    ran build

--- script.js
+++ script.js
@@ -80,5 +80,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port:${port}`);
 });
